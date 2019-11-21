12b381c005cc88612f8d8a503c68b0bd7cbbae65
Sherry WeMott <sherry.wemott@gmail.com>
2019-11-21 13:23:40 -0700

    read in data

--- writing/hw5.r
+++ writing/hw5.r
@@ -24,6 +24,14 @@ denver_zip <- zctas(cb = TRUE, starts_with =
                       c("802"), class = "sf")
 plot(denver_zip)
 
+# facet into solved and unsolved
+denver_disp <- denver_race %>% 
+  mutate(disposition = factor(disposition, levels = c("Closed without arrest", 
+                                                      "Closed by arrest", 
+                                                      "Open/No arrest"),
+                              labels = c("solved", "solved", "unsolved")))
+
+
 # create map
 denver_crs <- denver_disp %>% 
   filter(!is.na(lat)) %>% 
@@ -34,22 +42,17 @@ denver_crs
 # create plot 
 zip_map <- ggplot() +
   geom_sf(data = denver_zip, color = "lightgray") +
-  geom_sf(data = denver_crs, aes(color = factor(victim_race), 
-                                 shape = disposition)) +
-            facet_wrap(~disposition, ncol =1)
+  geom_sf(data = denver_crs, aes(color = factor(victim_race)), 
+          show.legend = 'point') +
+  facet_wrap(~disposition, ncol =1) +
+  ggtitle("Homicide cases in Denver, CO (by zipcode)") +
+  labs(x = "Longitude", y = "Lattitude") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  labs(colour = "Victim Race")
+  
 zip_map
 
-# facet into solved and unsolved
-denver_disp <- denver_race %>% 
-  mutate(disposition = factor(disposition, levels = c("Closed without arrest", 
-                                                      "Closed by arrest", 
-                                                      "Open/No arrest"),
-                              labels = c("solved", "solved", "unsolved")))
-denver_disp
-  mutate(fct_lump(disposition, n = 2)) %>% # convert dispositions to factors
-  count(disposition)
-  mutate(is_unsolved = !(disposition == "Closed by arrest"),
-         is_solved = (disposition == "Closed by arrest"))
+
 
 
 
